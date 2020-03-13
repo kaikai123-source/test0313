2f4ec03ee24d1f345e8a5894b5d63a2ab9689568
shengkai <Administrator@DESKTOP-539KRNO>
2020-03-13 12:15:45 +0800

    添加项目文件。

---
+++ Project1/main1.cpp
@@ -0,0 +1,19 @@
+#include<opencv2/opencv.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
+int main() {
+cv:Mat src_color = imread("D://1.png");
+std:vector<cv::Mat>channels;
+	cv::split(src_color, channels);
+	cv::Mat B = channels.at(0);
+	cv::Mat G = channels.at(1);
+	cv::Mat R = channels.at(2);
+	cv::imshow("red",R);
+	cv::imshow("green",G );
+	cv::imshow("bule", B);
+	cv::imshow("orignal Mat", src_color);
+	waitKey(0);
+
+}
